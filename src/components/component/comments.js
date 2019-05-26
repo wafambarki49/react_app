7d8d116de8fdafee4e973a731c4791a1a2326ea5
wafambarki49 <wafa.mbarki@esprit.tn>
2019-05-26 23:44:28 +0200

    handle errors and loading

--- src/components/component/comments.js
+++ src/components/component/comments.js
@@ -1,41 +1,65 @@
-import React, {Component} from 'react';
-class Comments extends Component{
+import React, { Component } from 'react';
+class Comments extends Component {
 
 
-constructor(props) {
+  constructor(props) {
     super(props);
-    
+
     this.state = {
-      comment: []
+      comment: [],
+      err: false,
+      loading: true
     };
   }
-componentDidMount() {
-  const id = this.props.match.params.id;
-  fetch('https://jsonplaceholder.typicode.com/comments'+'?postId='+id)
-      .then(response => response.json())
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    fetch('https://jsonplaceholder.typicode.com/comments?postId=' + id)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
       .then(comment => {
-               this.setState({ comment })
-    });
+        this.setState({ comment, loading: false, err: false })
+      })
+      .catch(err => {
+        this.setState({ err: true })
+      })
+
   }
-render(){
 
-return  (
-<div>
-<ul>
-        {this.state.comment.map(hit =>
-          <li key={hit.id}>
-            
-            <p className="text-info">Email <span className="text-muted">{hit.email}</span></p>
-            <p className="text-warning"> {hit.body}</p>
-          </li>
-        )}
-      </ul>
 
-</div>
-     
- 
-);
 
-}
+  render() {
+    let commentsView = <p>please wait while loading....</p>
+
+    if (this.state.err) {
+      commentsView = <p>Something Went Wrong</p>
+    }
+
+
+    if (this.state.loading === false && this.state.err === false) {
+      commentsView = this.state.comment.map(hit =>
+        <li key={hit.id}>
+          <p className="text-info">Email <span className="text-muted">{hit.email}</span></p>
+          <p className="text-warning"> {hit.body}</p>
+        </li>
+      )
+    }
+
+
+    return (
+      <div>
+        <ul>
+          {commentsView}
+        </ul>
+
+      </div>
+
+
+    );
+
+  }
 }
 export default Comments;
--- src/components/component/posts.js
+++ src/components/component/posts.js
@@ -6,34 +6,53 @@ class Posts extends Component {
     super(props);
 
     this.state = {
-      data: {}
-
+      data: {},
+      loading: true,
+      err: false
     };
   }
 
   navigateToComments() {
-    const id  = this.state.data.id;
-    this.props.history.push('/'+id+'/comments')
+    const id = this.state.data.id;
+    this.props.history.push('/' + id + '/comments')
   }
 
   componentDidMount() {
     const id = Math.floor(Math.random() * 100) + 1;
-    console.log(this.id);
-    fetch('https://jsonplaceholder.typicode.com/posts' + '/' + id)
-      .then(response => response.json())
+    fetch('https://jsonplaceholder.typicode.com/post/' + id)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
       .then(data => {
-        this.setState({ data })
-      });
+        this.setState({ data, loading: false, err: false })
+      })
+      .catch(error => this.setState({ err: true }));
   }
 
   render() {
 
+    let postView = <p>please wait while loading....</p>
+
+    if (this.state.err) {
+      postView = <p>Something Went Wrong</p>
+    }
+
+    if (!this.state.loading && !this.state.err) {
+      postView =
+        <div>
+          <h1 className="text-success">{this.state.data.title}</h1>
+          <p className="text-info">{this.state.data.body}</p>
+          <button className="btn btn-primary" onClick={this.navigateToComments.bind(this)}>Show Comments</button>
+        </div>
+    }
+
+
     return (
       <div>
-
-        <h1 className="text-success">{this.state.data.title}</h1>
-        <p className="text-info">{this.state.data.body}</p>
-        <button className="btn btn-primary" onClick={this.navigateToComments.bind(this)}>Show Comments</button>
+        {postView}
       </div>
 
 
