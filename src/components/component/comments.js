1269b57479ca2625910736cb41bbbc3a8e30a375
wafambarki49 <wafa.mbarki@esprit.tn>
2019-05-27 16:54:22 +0200

    add hooks to react app

--- src/components/component/comments.js
+++ src/components/component/comments.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import {ListGroup,ListGroupItem,ListGroupItemHeading,ListGroupItemText} from 'reactstrap';
-class Comments extends Component {
+const Comments = (props) =>  {
 
+  const [comments,setComments] = useState([]);
+  const [err,setErr] = useState(false);
+  const [loading,setLoading] = useState(true);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      comment: [],
-      err: false,
-      loading: true
-    };
-  }
-  componentDidMount() {
-    const id = this.props.match.params.id;
+  const getComments = () => {
+    const id = props.match.params.id;
     fetch('https://jsonplaceholder.typicode.com/comments?postId=' + id)
       .then(response => {
         if (!response.ok) {
@@ -22,26 +16,30 @@ class Comments extends Component {
         return response.json();
       })
       .then(comment => {
-        this.setState({ comment, loading: false, err: false })
+        setComments(comment)
+        setErr(false)
+        setLoading(false)
       })
       .catch(err => {
-        this.setState({ err: true })
+        setErr(true)
       })
-
   }
 
+  useEffect(() => {
+    getComments()
+  },[])
+  
 
 
-  render() {
     let commentsView = <p>please wait while loading....</p>
 
-    if (this.state.err) {
+    if (err) {
       commentsView = <p>Something Went Wrong</p>
     }
 
 
-    if (this.state.loading === false && this.state.err === false) {
-      commentsView = this.state.comment.map(hit =>
+    if (!loading && !err ) {
+      commentsView = comments.map(hit =>
         <ListGroupItem  key={hit.id}>
           <ListGroupItemHeading className="text-info">Email <span className="text-muted">{hit.email}</span></ListGroupItemHeading>
           <ListGroupItemText className="text-warning"> {hit.body}</ListGroupItemText>
@@ -58,6 +56,6 @@ class Comments extends Component {
 
     );
 
-  }
+  
 }
 export default Comments;
--- src/components/component/posts.js
+++ src/components/component/posts.js
@@ -1,65 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'reactstrap';
-class Posts extends Component {
+const Posts = (props) => {
 
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [err, setError] = useState(false);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: {},
-      loading: true,
-      err: false
-    };
-  }
-
-  navigateToComments() {
-    const id = this.state.data.id;
-    this.props.history.push('/' + id + '/comments')
-  }
-
-  componentDidMount() {
+  const getData = () => {
     const id = Math.floor(Math.random() * 100) + 1;
     fetch('https://jsonplaceholder.typicode.com/posts/' + id)
       .then(response => {
         if (!response.ok) {
-          throw Error(response.statusText); 
+          throw Error(response.statusText);
         }
         return response.json();
       })
       .then(data => {
-        this.setState({ data, loading: false, err: false })
+        setData(data);
+        setError(false);
+        setLoading(false);
       })
-      .catch(error => this.setState({ err: true }));
+      .catch(error => setError(true));
   }
 
-  render() {
+  const navigateToComments = () => {
+    const id = data.id;
+    props.history.push('/' + id + '/comments')
+  }
 
-    let postView = <p>please wait while loading....</p>
-
-    if (this.state.err) {
-      postView = <p className="text-warning">Something Went Wrong</p>
-    }
-
-    if (!this.state.loading && !this.state.err) {
-      postView =
-        <div>
-          <h1 className="text-success">{this.state.data.title}</h1>
-          <p className="text-info">{this.state.data.body}</p>
-          <Button color="primary" onClick={this.navigateToComments.bind(this)}>Show Comments</Button>
-          
-        </div>
-    }
+  useEffect(() => {
+    getData();
+  }, {});
 
 
-    return (
+
+  let postView = <p>please wait while loading....</p>
+
+  if (err) {
+    postView = <p className="text-warning">Something Went Wrong</p>
+  }
+
+  if (!loading && !err) {
+    postView =
       <div>
-        {postView}
+        <h1 className="text-success">{data.title}</h1>
+        <p className="text-info">{data.body}</p>
+        <Button color="primary" onClick={navigateToComments.bind(this)}>Show Comments</Button>
+
       </div>
-
-
-    );
-
   }
+
+
+  return (
+    <div>
+      {postView}
+    </div>
+
+
+  );
+
+
 }
 export default Posts;
