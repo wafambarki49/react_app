1807f727f391279a9747c3a139a17c5722e06cfb
wafambarki49 <wafa.mbarki@esprit.tn>
2019-07-29 00:48:38 +0200

    change creating store to Index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 
 import './App.css';
 import RoutesApp from './routes';
-import AppReducer from './context/reducer';
-import thunk from "redux-thunk";
-
-
 
 function App() {
 
-  const store = createStore(AppReducer,compose(applyMiddleware(thunk)));
-
   return (
-    <Provider store={store}>
     <div className="App container">
       <RoutesApp />
     </div>
-    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import AppReducer from './context/reducer';
+import thunk from "redux-thunk";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(AppReducer, compose(applyMiddleware(thunk)));
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
